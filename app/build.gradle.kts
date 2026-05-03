97c8158e27fa6823bf5c45c88dc938438e4b90ba
gojoyoFo <javapro15912@gmail.com>
2026-05-03 15:14:35 +0700

    wokwok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,7 +112,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
     implementation("com.google.firebase:firebase-analytics")
-    implementation "androidx.work:work-runtime-ktx:2.9.0"
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 }
 
 // ── Auto-copy fps_core binary ke assets setelah CMake build ──────────────────

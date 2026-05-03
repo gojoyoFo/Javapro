9a810e378da05933d187762fafda568590aa2249
gojoyoFo <javapro15912@gmail.com>
2026-05-03 12:30:40 +0700

    wokwok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,9 +108,10 @@ dependencies {
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 // ── Auto-copy fps_core binary ke assets setelah CMake build ──────────────────
